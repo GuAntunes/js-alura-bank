152a91b22d8876f56843eb1b3ed2c69ed6958b10
Gustavo Antunes <GuAntunes@github.com>
2020-09-14 23:07:22 -0300

    Instalando TypeScript Definitions

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -5,15 +5,15 @@ class NegociacaoController {
         this._mensagemView = new MensagemView('#mensagemView');
         //Realização do casting do tipo 'Element' que é retornado pelo método
         // querySelector para o HTMLInputElement 
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         //Previne que a tela seja recarregada
         event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
         this._negociacoes.adiciona(negociacao);
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update("Negociação adicionada com sucesso!");
